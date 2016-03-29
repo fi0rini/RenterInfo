58a11b8e7c0e6a11d3109879b76a2822186e7d33
nick fiorini <nf071590@gmail.com>
2016-03-29 12:21:33 -0400

    more lint fixes and ignore files

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const exec = require('child_process').exec;
 const execFile = require('child_process').execFile;
 
-const apiProc = execFile('node', ['.'], {cwd: './apiserver'});
+const apiProc = execFile('node', ['.'], { cwd: './apiserver' });
+
 apiProc.stdout.on('data', (data) => process.stdout.write(`${data}`));
 apiProc.stderr.on('data', (data) => process.stderr.write(`${data}`));
 
-const feProc = execFile('node', ['.'], {cwd: './frontend'});
+const feProc = execFile('node', ['.'], { cwd: './frontend' });
+
 feProc.stdout.on('data', (data) => process.stdout.write(`${data}`));
 feProc.stderr.on('data', (data) => process.stderr.write(`${data}`));
 
-const gulp = exec('gulp', {cwd: './frontend'});
+const gulp = exec('gulp', { cwd: './frontend' });
+
 gulp.stdout.on('data', (data) => process.stdout.write(`${data}`));
 gulp.stderr.on('data', (data) => process.stderr.write(`${data}`));
 
